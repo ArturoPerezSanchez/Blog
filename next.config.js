4565538f2f8dca05fd242010769fb81aa3dfbb54
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-15 22:06:40 +0200

    [build]: initial configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-};
-
-module.exports = nextConfig;
+}
