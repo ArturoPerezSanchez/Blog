c9a57fe14b0d87abfddb00be97b5ead72ae52402
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-25 22:46:15 +0200

    [style]: modified banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,16 @@
+import Image from 'next/image'
+
 export default function Banner() {
     return(
-        <header className="py-5 bg-light border-bottom mb-4">
-        <div className="container">
-            <div className="text-center my-5">
-                <h1 className="fw-bolder">Welcome to my blog home page </h1>
-                <p className="lead mb-0">Build nextjs blog website with Markdown, sitemap, serachbar, category, tag and SEO support </p>
+        <header className="bg-light border-bottom mb-4">
+        <div className="banner">
+            <div className="text-center">
+            <Image
+                src="/banner.png"
+                alt="The Geek Gazette Banner"
+                width={1800}
+                height={500}
+                />
             </div>
         </div>
     </header>
