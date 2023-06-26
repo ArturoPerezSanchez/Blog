c64855c3bbc7319799926be5e455677928728d8e
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-26 23:26:42 +0200

    [fix](banner): made responsive

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 
 export default function Banner() {
     return(
-        <header className="mb-4">
+        <header style={{  paddingTop: "55px"}} className="mb-4">
         <div className="banner bg-p">
-            <div style={{paddingTop: "55px", backgroundImage:"url('/banner.png')", backgroundSize: "100vw 350px", width:"100vw", height:"350px"}} >
+            <div className="banner-dimensions" style={{backgroundImage:"url('/banner.png')"}} >
             </div>
         </div>
     </header>
