e943691ed7bf36907ddfee78910b836288af112f
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-27 23:16:38 +0200

    [fix](banner): removed horizontal scroll bar

--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@
 export default function Banner() {
     return(
         <header style={{  paddingTop: "55px"}} className="mb-4">
-        <div className="banner bg-p">
+        <div className="banner bg-p overflow-hidden">
             <div className="banner-dimensions" style={{backgroundImage:"url('/banner.png')"}} >
             </div>
         </div>
