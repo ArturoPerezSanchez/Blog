f056a67ea415a893b8c7c6b0231f67e29651ceec
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-06-27 23:19:26 +0200

    [feat](posts-list): added infinite scroll

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
-
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import { NextSeo } from 'next-seo';
-import Post from '../components/Post'
-import Banner from "../components/Banner";
-import Topbar from "../components/Topbar"
-import { sortByDate, slugify,ImageUrl} from '../utils'
+import { useEffect, useState } from 'react';
+import Post from '../components/Post';
+import Banner from '../components/Banner';
+import Topbar from '../components/Topbar';
+import { sortByDate, ImageUrl } from '../utils';
 
-export default function Home({ posts }) {
+export default function Home({ initialPosts }) {
+  const [displayedPosts, setDisplayedPosts] = useState(5);
+  const [allPosts, setAllPosts] = useState(initialPosts);
 
+  const handleLoadMore = () => {
+    setDisplayedPosts(prevCount => prevCount + 3); // Increase the count to load more posts
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+        displayedPosts < allPosts.length
+      ) {
+        handleLoadMore();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [displayedPosts, allPosts]);
 
   return (
     <div>
@@ -30,86 +51,64 @@ export default function Home({ posts }) {
             },
           ],
           site_name: 'The Geek Gazette',
-        }}      
+        }}
       />
-        <Banner /> 
+      <Banner />
       <div className="container">
-      <Topbar />
+        <Topbar />
         <div className="row">
-
           <div className="col-lg-8">
-
-            {posts.map((post, index) => (
+            {allPosts.slice(0, displayedPosts).map((post, index) => (
               <Post key={index} post={post} />
             ))}
-
-
           </div>
-
-
-          
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
   // Get files from the posts dir
-  const files = fs.readdirSync(path.join('posts'))
-
- 
-
-  
+  const files = fs.readdirSync(path.join('posts'));
 
   // Get slug and frontmatter from posts
-  const tempPosts = files.map((filename) => {
+  const tempPosts = files.map(filename => {
     // Create slug
-    const slug = filename.replace('.md', '')
+    const slug = filename.replace('.md', '');
 
     // Get frontmatter
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    )
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
 
-    const { data: frontmatter } = matter(markdownWithMeta)
-
+    const { data: frontmatter } = matter(markdownWithMeta);
 
     if (frontmatter.draft === false) {
       return {
         slug,
         frontmatter,
-      }
+      };
     } else {
-      return null
+      return null;
     }
+  });
 
-  })
+  //  remove null in tempPosts
+  const posts = tempPosts.filter(post => {
+    return post && post;
+  });
 
-  //  remove null in tempPosts 
-  const posts = tempPosts.filter(
-    post => {
-      return post && post
-    }
-  )
-  const jsonString = JSON.stringify(posts)
+  const jsonString = JSON.stringify(posts);
   fs.writeFileSync('./search.json', jsonString, err => {
     if (err) {
-        console.log('Error writing file', err)
+      console.log('Error writing file', err);
     } else {
-        console.log('Successfully wrote file')
+      console.log('Successfully wrote file');
     }
-})
+  });
 
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      initialPosts: posts.sort(sortByDate),
     },
-  }
-
-
+  };
 }
-
-
- 
