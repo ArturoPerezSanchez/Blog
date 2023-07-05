6ccf1e6e06ef6333d09de0075fe6c15dc6ac7e93
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-07-06 00:29:38 +0200

    [style](posts): new posts list styles

--- components/Post.js
+++ components/Post.js
@@ -1,35 +1,38 @@
 import Link from 'next/link'
 import { slugify } from '../utils'
+import tagColors from "../public/tagColors";
 
 export default function Post({ post }) {
 
   const date = new Date(post.frontmatter?.date)
   return (
     <div className="card mb-4">
-      <a href={`/blog/${post.slug}`} > <img className="card-img-top" src={post.frontmatter.image} alt="..." /></a>
-      <div className="card-body">
-        <div className="small text-muted">{`${date.getMonth() + 1} - ${date.getDate()} - ${date.getFullYear()}`}</div>
-       
-        <div> {
-                  post.frontmatter.tags.map(
-                    tag => {
+      <div className="card-container tertiary-background">
+        <div className="cardContent">
 
-                      const slug = slugify(tag)
-
-                      return (<Link key={tag} href={`/tag/${slug}`}>
-                        <a className='btn'>
-                          <h6 className=' post-title'>#{tag}</h6>
-                        </a>
-                      </Link>)
-                    }
-                  )
-                } </div>
-        <h2 className="card-title">{post.frontmatter.title}</h2>
-        <p className="card-text">{post.frontmatter.summary}</p>
-        <Link href={`/blog/${post.slug}`}>
-          <a className='btn'>Read More</a>
-        </Link>
+          <p className="card-date">{`${date.getMonth() + 1} - ${date.getDate()} - ${date.getFullYear()}`}</p>
+          
+          <h2 className="card-title"><a href={`/blog/${post.slug}`} >{post.frontmatter.title}</a></h2>
+          <p className="card-subtitle"> {post.frontmatter.summary}</p>
+          <div className="tags-container">
+            <div className="mb-0 tag-list"> 
+              { post.frontmatter.tags.map((tag) => {
+                const slug = slugify(tag);
+                const tagColor = tagColors[tag.toLowerCase()] || "#808080";
+                  return (
+                    <div className="tag-item" style={{ backgroundColor: tagColor }} key={tag}>
+                      <Link href={`/tag/${slug}`}>
+                        <a>#{tag}</a>
+                      </Link>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+        <div style={{backgroundImage: `url(${post.frontmatter.image})`}} className="card-img-container" onClick={() => {window.location.href=`/blog/${post.slug}`}}></div>
       </div>
     </div>
+
   )
 }
